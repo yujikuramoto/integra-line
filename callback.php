5a8a6ec728538139c594fc46b99dd92701b8cce8
yujikuramoto <37259497+yujikuramoto@users.noreply.github.com>
2020-07-15 23:38:34 +0900

    Add files via upload

--- callback.php
+++ callback.php
@@ -1,9 +1,54 @@
 ﻿<?php 
 $accessToken = 'g+1NNYuFz0EDbgfBIP13UCjol3b04Rs793Q5GQ8Us8Fqpt5lFJJ23dtKkoQMysx+PVpnev2rkZ6g94tMNa5ZvUixtnd3NbEh0mY/3W1ztUr7ILa7G4NX/iFC+ovgjohW9QsJZIG6TcgkI8a43GMQ3wdB04t89/1O/w1cDnyilFU='; 
 $jsonString = file_get_contents('php://input'); error_log($jsonString); 
-$jsonObj = json_decode($jsonString); $message = $jsonObj->{"events"}[0]->{"message"}; 
+$jsonObj = json_decode($jsonString);
+$message = $jsonObj->{"events"}[0]->{"message"}; 
+$userID = $jsonObj->{"events"}[0]->{"source"};
 $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 
+$endMsg = 0;
+
+if ($message->{"text"} == '安否確認'){
+
+    $messageData = [ 
+        'type' => 'template', 
+        'altText' => '安否確認', 
+        'template' => [ 'type' => 'buttons', 
+            'thumbnailImageUrl' => 'https://liginc.co.jp/wp-content/uploads/2020/05/ec_200507_01_h-1310x874.jpg', 
+            'imageAspectRatio' => 'rectangle', 
+            'imageSize' => 'cover',          
+            'imageBackgroundColor' => '#FFFFFF',          
+            'text' => '現在状況を教えて下さい。', 
+            'actions' => [
+                [ 'type' => 'message', 'label' => '怪我はありません。', 'text' => '回答1:1' ],
+                [ 'type' => 'message', 'label' => '怪我がありますが、対応できます', 'text' => '回答1:2' ], 
+                [ 'type' => 'message', 'label' => 'レスキューが必要です', 'text' => '回答1:3' ], 
+            ] 
+        ]
+ ];
+ endMsg = 1;
+}
+
+
+if ($message->{"text"} == '回答1:1' || $message->{"text"} == '回答1:2' ||  $message->{"text"} == '回答1:3'){
+
+    $messageData = [ 
+        'type' => 'template', 
+        'altText' => '住所確認', 
+        'template' => [ 'type' => 'buttons', 
+            'thumbnailImageUrl' => 'https://liginc.co.jp/wp-content/uploads/2020/05/ec_200507_01_h-1310x874.jpg', 
+            'imageAspectRatio' => 'rectangle', 
+            'imageSize' => 'cover',          
+            'imageBackgroundColor' => '#FFFFFF',          
+            'text' => '現在いる場所を教えて下さい。', 
+            'actions' => [
+                [ 'type' => 'uri', 'label' => '地図より選択', 'uri' => 'https://line.me/R/nv/location/' ],
+                [ 'type' => 'message', 'label' => '位置を入力', 'text' => '位置入力' ], 
+            ] 
+        ]
+ ];
+ endMsg = 1;
+}
 
  // 送られてきたメッセージの中身からレスポンスのタイプを選択 
 if ($message->{"text"} == '確認') {
@@ -85,9 +130,16 @@ if ($message->{"text"} == '確認') {
             ] 
     ];
  } else {
+
+    if(endMsg==0){
+
      // それ以外は送られてきたテキストをオウム返し
      $messageData = [ 'type' => 'text', 'text' => $message->{"text"} ]; 
+
+    }
+
 } 
+
 $response = [ 'replyToken' => $replyToken, 'messages' => [$messageData] ]; 
 error_log(json_encode($response)); 
 $ch = curl_init('https://api.line.me/v2/bot/message/reply'); 

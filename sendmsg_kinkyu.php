8f174b3a99bba44c13f51096aa6a0813633b3a89
yujikuramoto <37259497+yujikuramoto@users.noreply.github.com>
2020-12-04 16:40:41 +0900

    Add files via upload

--- sendmsg_kinkyu.php
+++ sendmsg_kinkyu.php
@@ -3,22 +3,40 @@ $accessToken = 'g+1NNYuFz0EDbgfBIP13UCjol3b04Rs793Q5GQ8Us8Fqpt5lFJJ23dtKkoQMysx+
 
 $userID=$_GET['uID'];
 $text = [
-  [
-    'type' => 'template',
-    'altText' => '安否確認をお願いします。',
-    'template' => [
-		'type' => 'buttons',
-		'actions' => [
-			'type' => 'message',
-			'label' => '安否確認を行う',
-			'text' => '安否確認'
-		],
-		'thumbnailImageUrl' => 'https://t4.ftcdn.net/jpg/02/33/71/53/240_F_233715387_fNNxz3L3re2Fh0nM3xLqMHTpJXCEMbms.jpg',
-		'title' => '災害が発生しました',
-		'text' => '緊急支援の為、下のボタンより安否確認を行ってください。'
+  // [
+  //   'type' => 'template',
+  //   'altText' => '安否確認をお願いします。',
+  //   'template' => [
+		// 'type' => 'buttons',
+		// 'actions' => [
+		// 	'type' => 'message',
+		// 	'label' => '安否確認を行う',
+		// 	'text' => '安否確認'
+		// ],
+		// 'thumbnailImageUrl' => 'https://t4.ftcdn.net/jpg/02/33/71/53/240_F_233715387_fNNxz3L3re2Fh0nM3xLqMHTpJXCEMbms.jpg',
+		// 'title' => '災害が発生しました',
+		// 'text' => '緊急支援の為、下のボタンより安否確認を行ってください。'
 
+  //   ]
+  // ],
+
+
+
+    'type'     => 'template',
+    'altText'  => '安否確認をお願いします。',
+    'template' => [
+        'type'    => 'buttons',
+        'thumbnailImageUrl' => 'https://t4.ftcdn.net/jpg/02/33/71/53/240_F_233715387_fNNxz3L3re2Fh0nM3xLqMHTpJXCEMbms.jpg',
+        'title'   => '災害が発生しました' ,
+        'text'    => '緊急支援の為、下のボタンより安否確認を行ってください。',
+        'actions' => [
+            ['type'=>'message', '安否確認を行う'=>'', 'text'=>'安否確認',],
+        ],
     ]
-  ],
+
+
+
+
 ];
 
 $message = [

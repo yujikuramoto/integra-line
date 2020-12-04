204167b93a110a2c58b28bf4c94e90fbe876872c
yujikuramoto <37259497+yujikuramoto@users.noreply.github.com>
2020-12-04 15:08:04 +0900

    Add files via upload

--- home/index.php
+++ home/index.php
@@ -4,7 +4,7 @@
 
     $userID=$_GET['uID'];
     // リダイレクト先のURLへ転送する
-    $url = 'https://ostomate.site/regist/regist?uid=' . $_GET['uID'];
+    $url = 'https://ostomate.site/regist/regist?uid=' . $userID;
     header('Location: ' . $url, true, 301);
 
     // すべての出力を終了

b0e72ac7c03eff11c4b93201073236f9c6753ada
yujikuramoto <37259497+yujikuramoto@users.noreply.github.com>
2020-12-04 11:23:42 +0900

    Add files via upload

---
+++ sendmsg.php
@@ -0,0 +1,29 @@
+<?php
+$accessToken = 'g+1NNYuFz0EDbgfBIP13UCjol3b04Rs793Q5GQ8Us8Fqpt5lFJJ23dtKkoQMysx+PVpnev2rkZ6g94tMNa5ZvUixtnd3NbEh0mY/3W1ztUr7ILa7G4NX/iFC+ovgjohW9QsJZIG6TcgkI8a43GMQ3wdB04t89/1O/w1cDnyilFU='; 
+
+$userID=$_GET['uID'];
+
+$text = [
+    [
+    'type' => 'text',
+    'text' => 'hello world'
+    ],
+];
+
+$message = [
+    'to' => $userID,
+    'messages' => $text,
+    'client_id'     => $client_id,
+    'client_secret' => $client_secret
+];
+
+$message = json_encode($message);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $accessToken, 'Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_URL, 'https://api.line.me/v2/bot/message/push');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$message = curl_exec($ch);
+curl_close($ch);

2161e087f9a20c2596766b70b1921ca106e021ab
yujikuramoto <37259497+yujikuramoto@users.noreply.github.com>
2021-01-15 01:40:10 +0900

    Add files via upload

--- loginsample/config.php
+++ loginsample/config.php
@@ -1,8 +1,8 @@
 <?php
 // 直接アクセスを拒否する
 if (array_shift(get_included_files()) !== __FILE__) {
-    define("CLIENT_ID", "1655577549");
-    define("CLIENT_SECRET", "688dc45bc090c7a5d3c1d4ddb66df3dc");
+    define("CLIENT_ID", "1654489742");
+    define("CLIENT_SECRET", "2e8ca695b0fcb0662f497a328c87cdfe");
     define("REDIRECT_URI", "https://integra-line.herokuapp.com/loginsample/callback.php");
 } else {
     echo 'Access Denied';

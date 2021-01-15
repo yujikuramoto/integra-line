bd4ac1c41e2a25c543c98dca5eb5a4ebe7b8b629
yujikuramoto <37259497+yujikuramoto@users.noreply.github.com>
2021-01-15 10:01:53 +0900

    Add files via upload

--- home/index.php
+++ home/index.php
@@ -3,8 +3,25 @@
 <?php
 
     $userID=$_GET['uID'];
+
+try {
+	$goto=$_GET['goto'];
+} catch ( Exception $ex ) {
+	$goto='';
+}
+
+
     // リダイレクト先のURLへ転送する
-    $url = 'https://ostomate.site/regist/regist?uid=' . $userID;
+
+	if ($goto=='anpi')
+	{
+		
+    	$url = 'https://ostomate.site/emergency/step1?uid=' . $userID;
+	} else {
+    	$url = 'https://ostomate.site/regist/regist?uid=' . $userID;
+	}
+
+
     header('Location: ' . $url, true, 301);
 
     // すべての出力を終了
